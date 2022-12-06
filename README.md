e20a64972cb161e663a2858bc6844396b8b73d63
KayVasconcelos <84106035+KayVasconcelos@users.noreply.github.com>
2022-12-06 18:16:03 -0300

    Criei o site

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Seleção Brasileira - 2022
  Um site sobre a copa de 2022 no Catar, onde o usuário pode pesquisar o número da camisa do jogador parra receber mais informações sobre ele.
+ 
---
+++ site/script.js
@@ -0,0 +1,111 @@
+function busca(){
+    var jogador = document.querySelector('#jogador').value
+    var resp = document.querySelector('#resp')
+  
+    if(jogador==1){
+      resp.innerText = "Alisson Ramses Becker é um futebolista brasileiro que atua como goleiro. Atualmente joga pelo Liverpool. Ao se transferir ao Liverpool, Alisson se tornou o goleiro mais caro da história do futebol ultrapassando Buffon quando se transferiu do Parma com destino a Juventus por 54,2 milhões de euros."
+    }
+  
+    if(jogador==2){
+      resp.innerText = 'Danilo Luiz da Silva, mais conhecido por apenas Danilo, é um futebolista brasileiro que atua como lateral-direito. Atualmente, joga pela Juventus.'
+    }
+  
+    if(jogador==3){
+      resp.innerText = 'Thiago Emiliano da Silva é um futebolista brasileiro que atua como zagueiro. Atualmente joga no Chelsea. Thiago Silva começou sua carreira profissional em 2003, jogando como volante no RS Futebol; no ano seguinte, pelo Juventude, foi que passou a atuar como zagueiro.'
+    }
+  
+    if(jogador==4){
+      resp.innerText = 'Marcos Aoás Corrêa, mais conhecido como Marquinhos, é um futebolista luso-brasileiro que atua como zagueiro. Atualmente joga no Paris Saint-Germain. Ele começou sua carreira no Corinthians e, após vencer a Copa Libertadores da América de 2012, foi para a Roma por € 3 milhões.'
+    }
+  
+    if(jogador==5){
+      resp.innerText = 'Carlos Henrique Casimiro, mais conhecido como Casemiro, é um futebolista brasileiro que atua como volante. Atualmente joga no Manchester United.'
+    }
+  
+    if(jogador==6){
+      resp.innerText = 'Alex Sandro Lobo Silva, mais conhecido apenas como Alex Sandro, é um futebolista brasileiro que atua como lateral-esquerdo. Atualmente joga na Juventus.'
+    }
+  
+    if(jogador==7){
+      resp.innerText = 'Lucas Tolentino Coelho de Lima, mais conhecido como Lucas Paquetá, é um futebolista brasileiro que atua como meio-campista. Atualmente joga no West Ham.'
+    }
+  
+    if(jogador==8){
+      resp.innerText = 'Frederico Rodrigues de Paula Santos, mais conhecido como Fred, é um futebolista brasileiro que atua como volante. Atualmente joga no Manchester United.'
+    }
+  
+    if(jogador==9){
+      resp.innerText = 'Richarlison de Andrade, mais conhecido apenas como Richarlison, é um futebolista brasileiro que atua como atacante. Atualmente joga no Tottenham.'
+    }
+  
+    if(jogador==10){
+      resp.innerText = 'Neymar da Silva Santos Júnior, mais conhecido apenas como Neymar, é um futebolista brasileiro que atua como atacante. Atualmente joga pelo Paris Saint-Germain e pela Seleção Brasileira. É considerado o principal futebolista brasileiro da atualidade e um dos melhores futebolistas do mundo.'
+    }
+  
+    if(jogador==11){
+      resp.innerText = 'Raphael Dias Belloli, mais conhecido como Raphinha, é um futebolista brasileiro que atua como ponta. Atualmente, joga pelo Barcelona.'
+    }
+  
+    if(jogador==12){
+      resp.innerText = 'Weverton Pereira da Silva, mais conhecido apenas como Weverton, é um futebolista brasileiro que atua como goleiro. Atualmente, defende o Palmeiras.'
+    }
+  
+    if(jogador==13){
+      resp.innerText = 'Daniel Alves da Silva é um futebolista brasileiro que atua como lateral-direito ou meio-campista. Atualmente joga no Pumas, do México. Com 43 títulos conquistados, Daniel Alves é o jogador com mais títulos oficiais na história do futebol.'
+    }
+  
+    if(jogador==14){
+      resp.innerText = 'Éder Gabriel Militão, mais conhecido como Éder Militão ou simplesmente Militão, é um futebolista brasileiro que atua como zagueiro. Atualmente joga pelo Real Madrid.'
+    }
+  
+    if(jogador==15){
+      resp.innerText = 'Fábio Henrique Tavares, mais conhecido como Fabinho, é um futebolista brasileiro que atua como volante. Atualmente joga pelo Liverpool.'
+    }
+  
+    if(jogador==16){
+      resp.innerText = 'Alex Nicolao Telles, mais conhecido como Alex Telles, é um futebolista brasileiro que atua como lateral-esquerdo. Atualmente defende o Sevilla, emprestado pelo Manchester United.'
+    }
+  
+    if(jogador==17){
+      resp.innerText = 'Bruno Guimarães Rodrigues Moura é um futebolista brasileiro que atua como volante. Atualmente joga pelo Newcastle United.'
+    }
+  
+    if(jogador==18){
+      resp.innerText = 'Gabriel Fernando de Jesus, mais conhecido como Gabriel Jesus, é um futebolista brasileiro que atua como atacante. Atualmente joga pelo Arsenal.'
+    }
+  
+    if(jogador==19){
+      resp.innerText = 'Antony Matheus dos Santos, mais conhecido apenas como Antony, é um futebolista brasileiro que atua como ponta-direita. Atualmente joga pelo Manchester United.'
+    }
+  
+    if(jogador==20){
+      resp.innerText = 'Vinícius José Paixão de Oliveira Júnior, mais conhecido como Vinícius Júnior ou Vini Jr, é um futebolista brasileiro que também possui a nacionalidade espanhola. Atualmente joga no Real Madrid, atuando como ponta-esquerda.'
+    }
+  
+    if(jogador==21){
+      resp.innerText = 'Rodrygo Silva de Goes, mais conhecido apenas como Rodrygo, é um futebolista brasileiro que atua como atacante. Atualmente joga no Real Madrid.'
+    }
+  
+    if(jogador==22){
+      resp.innerText = 'Éverton Augusto de Barros Ribeiro, mais conhecido como Éverton Ribeiro, é um futebolista brasileiro que atua como meio-campista e ponta-direita. Atualmente, defende o Flamengo e a Seleção Brasileira.'
+    }
+  
+    if(jogador==23){
+      resp.innerText = 'Ederson Lúcio Santana de Moraes, mais conhecido apenas como Ederson, é um futebolista brasileiro que atua como goleiro. Atualmente, joga pelo Manchester City.'
+    }
+  
+    if(jogador==24){
+      resp.innerText = 'Gleison Bremer Silva Nascimento, mais conhecido apenas como Bremer, é um futebolista brasileiro que atua como zagueiro. Atualmente joga na Juventus. Seu nome foi escolhido por seu pai em homenagem ao futebolista alemão Andreas Brehme.'
+    }
+  
+    if(jogador==25){
+      resp.innerText = 'Pedro Guilherme Abreu dos Santos, mais conhecido apenas como Pedro, é um futebolista brasileiro que atua como centroavante. Atualmente defende o Flamengo e a Seleção Brasileira.'
+    }
+  
+    if(jogador==26){
+      resp.innerText = 'Gabriel Teodoro Martinelli Silva é um futebolista brasileiro que atua como atacante. Atualmente joga pelo Arsenal.'
+    }
+    
+  }
+  
+  bt2.addEventListener('click', busca)
